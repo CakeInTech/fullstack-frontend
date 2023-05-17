ebfd90dba6435536852e824bcaa2ae81e4ba5c2c
Mohamed Abdulhakim <abdulhakimm280@gmail.com>
2023-05-17 13:14:55 +0200

    - Setup the provider for the store

--- src/index.js
+++ src/index.js
@@ -1,11 +1,11 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
-import './index.css';
+import { Provider } from 'react-redux';
 import App from './App';
+import store from './redux/store';
 
-const root = ReactDOM.createRoot(document.getElementById('root'));
-root.render(
-  <React.StrictMode>
+ReactDOM.createRoot(document.getElementById('root')).render(
+  <Provider store={store}>
     <App />
-  </React.StrictMode>
+  </Provider>
 );
